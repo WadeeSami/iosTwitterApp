aabf7335e5fe315775dfaac23aba6b6e201e00f7
Wadee Sami <wabuzant@endeavorpal.com>
2016-12-26 13:38:08 +0200

    Initial Commit

---
+++ TwitterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterApp
+//
+//  Created by Wadee Sami on 12/26/16.
+//  Copyright © 2016 Wadee AbuZant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterApp
+//
+//  Created by Wadee Sami on 12/26/16.
+//  Copyright © 2016 Wadee AbuZant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
